faff6ed05ae2180203b35db7a7042d9a6f652de5
manavgurnani21 <mgurnani@amazon.com>
2025-07-11 10:16:17 -0700

    trying to deploy application

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import { MapView } from "@aws-amplify/ui-react-geo";
